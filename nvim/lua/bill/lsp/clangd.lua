2eee9f4e7c643d510803f11bee2b30f7cde8fc4e
AlchemistX <hick4321@gmail.com>
2022-09-21 23:09:05 +0900

    fix : error when QUERY_DRIVER env variable is not exists

--- nvim/lua/bill/lsp/clangd.lua
+++ nvim/lua/bill/lsp/clangd.lua
@@ -1,1 +1,15 @@
-return {}
+local cmd = {}
+if os.getenv("QUERY_DRIVER") then
+	cmd = {
+		"clangd",
+		"-log=verbose",
+		"--query-driver=" .. os.getenv("QUERY_DRIVER"),
+	}
+else
+	cmd = { "clangd" }
+end
+
+return {
+	settings = {},
+	cmd = cmd,
+}
