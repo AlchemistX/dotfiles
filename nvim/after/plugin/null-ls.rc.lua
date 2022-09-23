de0836fc4627d9c8395dabcc72e9ff3904d0c041
AlchemistX <hick4321@gmail.com>
2022-09-24 08:24:40 +0900

    Update formatting rule of lua

--- nvim/after/plugin/null-ls.rc.lua
+++ nvim/after/plugin/null-ls.rc.lua
@@ -1,35 +1,37 @@
-local status, null_ls = pcall(require, "null-ls")
+local status, null_ls = pcall(require, 'null-ls')
 if not status then
-	return
+  return
 end
 
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+local augroup_format = vim.api.nvim_create_augroup('Format', { clear = true })
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	sources = {
-		--[[ null_ls.builtins.diagnostics.eslint_d.with({ ]]
-		--[[   diagnostics_format = '[eslint] #{m}\n(#{c})' ]]
-		--[[ }), ]]
-		--[[ null_ls.builtins.diagnostics.zsh ]]
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-		diagnostics.flake8,
-	},
-	on_attach = function(client, _)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.api.nvim_clear_autocmds({ buffer = 0, group = augroup_format })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup_format,
-				buffer = 0,
-				callback = function()
-					vim.lsp.buf.formatting_seq_sync()
-				end,
-			})
-		end
-	end,
+  sources = {
+    --[[ null_ls.builtins.diagnostics.eslint_d.with({ ]]
+    --[[   diagnostics_format = '[eslint] #{m}\n(#{c})' ]]
+    --[[ }), ]]
+    --[[ null_ls.builtins.diagnostics.zsh ]]
+    formatting.prettier.with({ extra_args = { '--no-semi', '--single-quote', '--jsx-single-quote' } }),
+    formatting.black.with({ extra_args = { '--fast' } }),
+    formatting.stylua.with({
+      extra_args = { '--indent-type=Spaces', '--indent-width=2', '--quote-style=AutoPreferSingle' },
+    }),
+    diagnostics.flake8,
+  },
+  on_attach = function(client, _)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_clear_autocmds({ buffer = 0, group = augroup_format })
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = augroup_format,
+        buffer = 0,
+        callback = function()
+          vim.lsp.buf.formatting_seq_sync()
+        end,
+      })
+    end
+  end,
 })
--- nvim/lua/bill/lsp/clangd.lua
+++ nvim/lua/bill/lsp/clangd.lua
@@ -1,17 +1,16 @@
 local cmd = {}
 -- QUERY_DRIVER is like as
 --    export QUERY_DRIVER=`realpath oe-workdir`/recipe-sysroot-native/usr/bin/arm-starfishmllib32-linux-gnueabi/arm-starfishmllib32-linux-gnueabi-g++
-if os.getenv("QUERY_DRIVER") then
-	cmd = {
-		"clangd",
-		"-log=verbose",
-		"--query-driver=" .. os.getenv("QUERY_DRIVER"),
-	}
+if os.getenv('QUERY_DRIVER') then
+  cmd = {
+    'clangd',
+    '--query-driver=' .. os.getenv('QUERY_DRIVER'),
+  }
 else
-	cmd = { "clangd" }
+  cmd = { 'clangd' }
 end
 
 return {
-	settings = {},
-	cmd = cmd,
+  settings = {},
+  cmd = cmd,
 }
