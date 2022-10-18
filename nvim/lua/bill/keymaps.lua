7bd1a6429485ef66147425da602e5be22d8b1084
AlchemistX <hick4321@gmail.com>
2022-10-18 20:26:59 +0900

    update keymap

--- nvim/lua/bill/keymaps.lua
+++ nvim/lua/bill/keymaps.lua
@@ -6,12 +6,12 @@ local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 -- Remap q as Nop
-keymap("", "q", "<Nop>", opts)
+keymap('', 'q', '<Nop>', opts)
 
 -- Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+keymap('', '<Space>', '<Nop>', opts)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Modes
 --   normal_mode = "n",
@@ -23,50 +23,50 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Do not yank with x
-keymap("n", "x", '"_x', opts)
+keymap('n', 'x', '"_x', opts)
 
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap('n', '<C-h>', '<C-w>h', opts)
+keymap('n', '<C-j>', '<C-w>j', opts)
+keymap('n', '<C-k>', '<C-w>k', opts)
+keymap('n', '<C-l>', '<C-w>l', opts)
 
-keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+keymap('n', '<leader>e', ':NvimTreeToggle<cr>', opts)
 
 -- Resize with arrows
-keymap("n", "<C-S-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-S-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-S-Left>", ":vertical resize +2<CR>", opts)
-keymap("n", "<C-S-Right>", ":vertical resize -2<CR>", opts)
+keymap('n', '<C-S-Up>', ':resize +2<CR>', opts)
+keymap('n', '<C-S-Down>', ':resize -2<CR>', opts)
+keymap('n', '<C-S-Left>', ':vertical resize +2<CR>', opts)
+keymap('n', '<C-S-Right>', ':vertical resize -2<CR>', opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap('n', '<Right>', ':bnext<CR>', opts)
+keymap('n', '<Left>', ':bprevious<CR>', opts)
 
 -- Insert --
 -- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
+keymap('i', 'jk', '<ESC>', opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap('v', '<', '<gv', opts)
+keymap('v', '>', '>gv', opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+keymap('v', '<A-j>', ':m .+1<CR>==', opts)
+keymap('v', '<A-k>', ':m .-2<CR>==', opts)
+keymap('v', 'p', '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap('x', 'J', ":move '>+1<CR>gv-gv", opts)
+keymap('x', 'K', ":move '<-2<CR>gv-gv", opts)
+keymap('x', '<A-j>', ":move '>+1<CR>gv-gv", opts)
+keymap('x', '<A-k>', ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+keymap('t', '<C-h>', '<C-\\><C-N><C-w>h', term_opts)
+keymap('t', '<C-j>', '<C-\\><C-N><C-w>j', term_opts)
+keymap('t', '<C-k>', '<C-\\><C-N><C-w>k', term_opts)
+keymap('t', '<C-l>', '<C-\\><C-N><C-w>l', term_opts)
