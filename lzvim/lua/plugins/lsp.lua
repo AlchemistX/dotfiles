2b6b3386b3a16b1672166f531bc93d322afaaa16
AlchemistX <hick4321@gmail.com>
2025-07-21 14:07:38 +0900

    update LazyVim config

---
+++ lzvim/lua/plugins/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    setup = {
+      clangd = function(_, opts)
+        local query_driver = os.getenv("QUERY_DRIVER")
+        if query_driver and #query_driver > 0 then
+          table.insert(opts.cmd, "--query-driver=" .. query_driver)
+        end
+
+        local clangd_ext_opts = require("lazy.core.plugin").values(
+          require("lazy.core.config").plugins["clangd_extensions.nvim"],
+          "opts",
+          false
+        )
+
+        require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, {
+          server = opts,
+        }))
+
+        return false
+      end,
+    },
+  },
+}
