390ebfac087a4d90c499a538117fb6a9e85a014d
AlchemistX <hick4321@gmail.com>
2025-07-21 18:53:58 +0900

    update LazyVim config

---
+++ lzvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,24 @@
+return {
+  "rebelot/kanagawa.nvim",
+  config = function()
+    require("kanagawa").setup({
+      transparent = true,
+      theme = "dragon",
+      overrides = function(colors)
+        return {
+          ["@markup.link.url.markdown_inline"] = { link = "Special" }, -- (url)
+          ["@markup.link.label.markdown_inline"] = { link = "WarningMsg" }, -- [label]
+          ["@markup.italic.markdown_inline"] = { link = "Exception" }, -- *italic*
+          ["@markup.raw.markdown_inline"] = { link = "String" }, -- `code`
+          ["@markup.list.markdown"] = { link = "Function" }, -- + list
+          ["@markup.list.checked.markdown"] = { link = "WarningMsg" }, -- > checked list item
+          ["@markup.quite.markdown"] = { link = "Error" }, -- > blockcode
+
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+        }
+      end,
+    })
+  end,
+}
